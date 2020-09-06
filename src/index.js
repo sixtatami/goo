fe399e7b48dac8fb231bf349cbe416323770d0ef
Andreas Kraft <kraftandi@gmail.com>
2020-09-06 15:19:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 export default function Goo() {
-  return <div>hello goodness</div>;
+  return <div>hello goodness suckers</div>;
 }
 
 render(<Goo />, document.getElementById("root"));
