6a2e73f52ef503ddfa8c35365031f0ae5576009d
Andreas Kraft <kraftandi@gmail.com>
2020-09-06 14:53:19 +0200

    no import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { useTrail, animated } from 'react-spring'
-import './styles.css'
+// import { useTrail, animated } from 'react-spring'
+// import './styles.css'
 
 // const fast = { tension: 1200, friction: 40 }
 // const slow = { mass: 10, tension: 200, friction: 50 }
