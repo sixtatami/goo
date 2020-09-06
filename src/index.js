bec57db29d4ef1eac40d3001ed060847f28f992a
Andreas Kraft <kraftandi@gmail.com>
2020-09-06 13:59:08 +0200

    add text

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const slow = { mass: 10, tension: 200, friction: 50 }
 const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
 
 export default function Goo() {
-  const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: i => (i === 0 ? fast : slow) }))
+  const [trail, set] = useTrail(3, () => ({ xy: [0, 0], config: (i) => (i === 0 ? fast : slow) }))
   return (
     <>
       <svg style={{ position: 'absolute', width: 0, height: 0 }}>
@@ -17,7 +17,8 @@ export default function Goo() {
           <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
         </filter>
       </svg>
-      <div className="hooks-main" onMouseMove={e => set({ xy: [e.clientX, e.clientY] })}>
+      trial
+      <div className="hooks-main" onMouseMove={(e) => set({ xy: [e.clientX, e.clientY] })}>
         {trail.map((props, index) => (
           <animated.div key={index} style={{ transform: props.xy.interpolate(trans) }} />
         ))}
